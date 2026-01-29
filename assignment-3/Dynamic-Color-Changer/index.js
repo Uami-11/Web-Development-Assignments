64c4c9c4a2fdae883d59fb18160c9ac7acece124
Nirwan <nirwanmaharjantuladhar@gmail.com>
2026-01-29 10:08:20 +0545

    Added an extra feature to the color picker

--- assignment-3/Dynamic-Color-Changer/index.js
+++ assignment-3/Dynamic-Color-Changer/index.js
@@ -1,10 +1,20 @@
 const colorPicker = document.querySelector("#color-picker");
 const area = document.querySelector(".change");
+const title = document.querySelector("#doc-title");
+ogTitle = title.textContent;
 
 colorPicker.addEventListener("input", function()
 {
     const color = this.value;
     area.style.backgroundColor = color;
+    if (color == "#ffe4c4")
+    {
+        title.textContent = "That's the same color as the background!!";
+    }
+    else 
+    {
+        title.textContent = ogTitle;
+    }
 })
 
 function resetColor()
